76d581bcae8509dd742746a1a9e1a498b5916b7f
mikaelbk <mikael.kiste@gmail.com>
2017-10-13 16:03:34 +0200

    vec3 class and gitkraken
    First commit from gitkraken and added the vec3 class

---
+++ README.md
@@ -0,0 +1,2 @@
+# fys3150-project3
+Project 3 in FYS3150 - Computational Physics
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(int argc, char const *argv[])
+{
+	int N = 50;
+	int M = 3;
+	double *x = new double[N];
+	double *y = new double[N];
+	double *z = new double[N];
+	double **r = new double 
+	return 0;
+}
